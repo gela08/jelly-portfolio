936388ff82aadf8d275a7e67d9ef92dcca063e00
gela08 <angelajelly.gardan@hcdc.edu.ph>
2025-12-16 02:48:50 +0800

    Completed Certification Page

--- src/pages/Certification.tsx
+++ src/pages/Certification.tsx
@@ -11,78 +11,87 @@ type Cert = {
 
 const certificationData: Cert[] = [
   {
-    title: "Introduction to C++ Programming",
+    title: "Introduction to C++ Programming Certificate",
     issuer: "SkillUp",
     year: "2025",
     image: "/certificates/cpp2025.png",
     description:
-      "Introductory C++ programming covering syntax, logic, and problem solving.",
+      "Certificate of Completion for completing an online programming course. Thank you, SkillUp!",
   },
   {
-    title: "Educational Tour (Cebu-Bohol)",
+    title: "Educational Tour (Cebu-Bohol) Certificate",
     issuer: "World of Adventures Travel and Tours",
     year: "2025",
     image: "/certificates/watt2025.png",
-    description: "",
+    description:
+      "Certificate of Completion after we finished touring with WATT. It was a really fun and unforgettable journey! We had a really cool and amazing tour guide who knows it all from cebu all the way to bohol. Thank you, WATT team! Kani siya diri is gi reclaim ni siya since WATT sign!!",
   },
   {
-    title: "National Service Training Program",
+    title: "National Service Training Program Certificate",
     issuer: "NSTP - CWTS",
     year: "2025",
     image: "/certificates/nstp2025.png",
-    description: "",
+    description:
+      "Certificate of Completion from our NSTP subject. Had a fun adventure during our Sasa Port visit and a graduation for completing the subject. Thank you to our NSTP teachers, and goodbye, NSTP subject!",
   },
   {
-    title: "Codechum",
+    title: "Codechum Certificate",
     issuer: "CC105 - Information Management",
     year: "2025",
     image: "/certificates/codechum2025.png",
-    description: "TBA",
+    description:
+      "Certificate of Completion from Codechum after answering all the activities from the class. Had a stressful time answering some questions, but it was really fun enhancing my programming skills—and also my cramming skills before the deadlines. The site had a lot of crashes and bugs; I hope they were able to fix them. Still got a good score there, btw. Thank you, Codechum!",
   },
   {
-    title: "PANAGSANKA2025 - Certificate of Recognition",
+    title: "PANAGSANKA2025 Certificate",
     issuer: "Physical Education",
     year: "2025",
     image: "/certificates/pe2025.png",
-    description: "TBA",
+    description:
+      "Certificate of Recognition from our PE event. Got the chance to be one of the leaders; received stress but it paid off with my volleyball girls team (Champion). It was a really fun experience and I got to use my leadership skills. It was the biggest event I’ve handled so far (almost 900 students?), and it paid off for me and my classmates since all of us got 100% grades. Thank you to our PE teachers, and goodbye, PE subject!",
   },
   {
-    title: "CET TechnoFair - IT Programming Contest",
-    issuer: "TEAM SUNG JE ROM",
+    title: "TEAM SUNG JE ROM Champion Certificate",
+    issuer: "CET TechnoFair - IT Programming Contest",
     year: "2025",
     image: "/certificates/champ2025.png",
-    description: "TBA",
+    description:
+      "Certificate of Recognition from CET TechnoFair, 2nd time champion after the 2024 programming competition. Different teammates this time since one of my previous teammates wasn’t allowed to compete, and I was the only one available that day. I didn’t actually help much (sorry, guys!), but the event was upgraded and they gave us snacks during the competition. Overall, I was just happy to be there and give moral support to my team. Thank you, TEAM SUNG JE ROM! Fun fact: the team name is named after a legend — Jerome Sagunday.",
   },
   {
-    title: "Academic Excellence Dean's Lister",
-    issuer: "Third Honor",
+    title: "Second Honor Certificate",
+    issuer: "Dean's Lister",
     year: "2025",
     image: "/certificates/dl2025.png",
-    description: "TBA",
+    description:
+      "Excellent Academic Proficiency Certificate during the 1st Semester of 2024–2025 as a 2nd year BSIT student. This award was unexpected since I was only third honor during the 1st semester of 2023–2024. Didn’t expect to reach this level, but still very grateful!",
   },
   {
-    title: "CET TechnoFair - Programming Competition (IT Category)",
-    issuer: "TEAM TERNARY",
+    title: "TEAM TERNARY Champion Certificate",
+    issuer: "CET TechnoFair - Programming Competition (IT Category)",
     year: "2024",
     image: "/certificates/champ2024.png",
-    description: "TBA",
+    description:
+      "Certificate of Recognition from CET TechnoFair. First time joining a contest and we won the title as champions!! It was a really cool experience—I didn’t know what the problems would be, but I’m thankful for my OG team!! LEZZGOOO!! They were the brains of the team who dealt with the logic of the competition. My role was to understand, take notes, and support them while they figured out how to solve the problems. I wasn’t able to contribute much, but they still hyped me up and included me until we got our awards. Thank you, TEAM TERNARY!",
   },
   {
     title: "Academic Excellence Dean's Lister",
     issuer: "Second Honor",
     year: "2024",
     image: "/certificates/dl2024.png",
-    description: "TBA",
+    description:
+      "Excellent Academic Proficiency Certificate during the 1st Semester of 2023–2024 as a 1st year BSIT student. This award came very unexpectedly since it was my first year in college—figuring things out with a new level of knowledge and serious adult stuff. I didn’t even know colleges had this kind of recognition, so I didn’t expect any reward since I thought my grades were meh. Still grateful as always for receiving this award.",
   },
   {
-    title: "JavaScript Basics",
+    title: "JavaScript Basics Certificate",
     issuer: "BitDegree",
     year: "2023",
     image: "/certificates/js2023.png",
     description:
-      "JavaScript fundamentals including variables, functions, loops, and DOM basics.",
+      "Certificate of Completion from BitDegree. This certificate was required by our subject, and all of us were required to have it as a ticket for our grades. It was my first-ever certificate in college. I got to know the basics of JavaScript and was able to use them in some of our subject projects at that time. Thank you, BitDegree!",
   },
 ];
+
 
 /* -------------------- Desktop tilt -------------------- */
 
